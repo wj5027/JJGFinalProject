42bc5345eea1ddf3864dbbdb77c639609f0d4780
matth <matth@DESKTOP-4LOEI0S>
2019-01-30 14:58:50 +0900

    기야오

--- src/main/java/com/kh/tsp/parkingceo/controller/ExchangeMoney.java
+++ src/main/java/com/kh/tsp/parkingceo/controller/ExchangeMoney.java
@@ -1,11 +1,17 @@
 package com.kh.tsp.parkingceo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.kh.tsp.parkingceo.model.service.ExchangeMoneyService;
+
 @Controller
 public class ExchangeMoney {
+	
+	@Autowired
+	ExchangeMoneyService es;
 
 	public ExchangeMoney() {
 		// TODO Auto-generated constructor stub
@@ -16,5 +22,13 @@ public class ExchangeMoney {
 		
 		return "parkingceo/exchange/ExchangeMoney";
 	}
+	
+	public String exchangeMoney() {
+		
+		int exchangeMoney = es.insertTestMethod();
+		
+		return null;
+	}
+	
 
 }
