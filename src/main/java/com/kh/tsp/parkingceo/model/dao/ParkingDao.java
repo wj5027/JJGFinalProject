8c1f137b097d463737ac04df38d57e8db409b396
matth <matth@DESKTOP-4LOEI0S>
2019-02-11 19:29:21 +0900

    임수철 2/11

--- src/main/java/com/kh/tsp/parkingceo/model/dao/ParkingDao.java
+++ src/main/java/com/kh/tsp/parkingceo/model/dao/ParkingDao.java
@@ -1,20 +1,39 @@
 package com.kh.tsp.parkingceo.model.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.mybatis.spring.SqlSessionTemplate;
 
+import com.kh.tsp.common.ParkingCeoPageInfo;
 import com.kh.tsp.customer.model.vo.Member;
 
 public interface ParkingDao {
 
 	int insertParkingCeo(SqlSessionTemplate sqlSession, Member m);
 
-	int selectInsertParkingListCount(SqlSessionTemplate sqlSession, Member m);
+	int selectParkingListCount(SqlSessionTemplate sqlSession, Member m);
 
 	int exchangeParkingListCount(SqlSessionTemplate sqlSession, Member m);
 
-	int insertCouponListCount(SqlSessionTemplate sqlSession, Member m);
+	int selectCouponListCount(SqlSessionTemplate sqlSession, Member m);
 
-	int insertExchangeMoneyListCount(SqlSessionTemplate sqlSession, Member m);
+	int selectExchangeMoneyListCount(SqlSessionTemplate sqlSession, Member m);
+
+	ArrayList<HashMap<String, Object>> selectParkingList(SqlSessionTemplate sqlSession, ParkingCeoPageInfo pi,
+			Member m);
+
+	ArrayList<HashMap<String, Object>> selectExchangeParkingList(SqlSessionTemplate sqlSession, ParkingCeoPageInfo pi,
+			Member m);
+
+	ArrayList<HashMap<String, Object>> selectCouponList(SqlSessionTemplate sqlSession, ParkingCeoPageInfo pi, Member m);
+
+	ArrayList<HashMap<String, Object>> selectExchangeMoneyList(SqlSessionTemplate sqlSession, ParkingCeoPageInfo pi,
+			Member m);
+
+
+
+
 
 
 }
