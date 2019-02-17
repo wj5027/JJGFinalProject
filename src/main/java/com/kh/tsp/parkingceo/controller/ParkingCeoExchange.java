626798f62fe3b2f5b96ac3f4fa2f1fd6d56ccb77
matth <matth@DESKTOP-4LOEI0S>
2019-02-17 19:06:16 +0900

    02-17 commit

--- src/main/java/com/kh/tsp/parkingceo/controller/ParkingCeoExchange.java
+++ src/main/java/com/kh/tsp/parkingceo/controller/ParkingCeoExchange.java
@@ -1,11 +1,25 @@
 package com.kh.tsp.parkingceo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.kh.tsp.customer.model.vo.Member;
+import com.kh.tsp.parkingceo.model.service.PromotionService;
+
 @Controller
 public class ParkingCeoExchange {
+	
+	
+	@Autowired
+	private PromotionService promotion;
 
 	public ParkingCeoExchange() {
 		
@@ -13,18 +27,19 @@ public class ParkingCeoExchange {
 	
 	
 	@RequestMapping(value="/parkingceoExchange.pc", method=RequestMethod.GET)
-	public String ParkingExchangePage() {
-		
-		
+	public String ParkingExchangePage(Model model,HttpSession session) {
+		Member m = (Member)session.getAttribute("loginUser");		
+		try {
+			//현재 보유중인 주차장 검색
+			ArrayList<HashMap<String, Object>> list = promotion.selectCurrentParkingList(m);
+			model.addAttribute("CurrentParkinglist", list);
+		}catch(Exception e) {
+			model.addAttribute("message", "주차장 조회 실패!");
+			return "common/errorPage";
+		}
 		return "parkingceo/exchange/ExchangePage";
 	}
 	
-	//실험용 메소드
-	@RequestMapping(value="/test.pc", method=RequestMethod.GET)
-	public String ParkingTest() {
-		
-		
-		return "parkingceo/exchange/tset";
-	}
+
 
 }
